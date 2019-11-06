791ae6afc10a77af0ec7a992be4db06128b745dc
Williams Aguilera <webnator@gmail.com>
2019-11-06 23:34:57 +0100

    Blah

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,88 @@
+<!-- <template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <CategoriesList></CategoriesList>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import CategoriesList from './components/CategoriesList';
+
+export default {
+  name: 'App',
+
+  components: {
+    CategoriesList,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script> -->
+
+
+
+
+
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+   <v-app id="app">
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        Capo music
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    
+  </v-app>
 </template>
 
 <style lang="scss">
--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="categories-list">
-    <div v-if="loading">
-      Loading...
-    </div>
-    <ul>
-      <li v-for="(category, index) of categories" :key="index">
-        {{ category }}
-      </li>
-    </ul>
+  
+    <v-row v-if="loading" :align="'center'">
+      <v-col><v-skeleton-loader ref="skeleton" :type="'chip'" class="mx-auto"></v-skeleton-loader></v-col>
+      <v-col><v-skeleton-loader ref="skeleton" :type="'chip'" class="mx-auto"></v-skeleton-loader></v-col>
+      <v-col><v-skeleton-loader ref="skeleton" :type="'chip'" class="mx-auto"></v-skeleton-loader></v-col>
+      <v-col><v-skeleton-loader ref="skeleton" :type="'chip'" class="mx-auto"></v-skeleton-loader></v-col>
+      <v-col><v-skeleton-loader ref="skeleton" :type="'chip'" class="mx-auto"></v-skeleton-loader></v-col>
+    </v-row>
+
+    <v-chip
+      v-for="(category, index) of categories" :key="index"
+      class="ma-2"
+      :input-value="selectedCategories.includes(category)"
+      outlined
+      ripple
+      filter
+      @click="toggleCategory(category)"
+    >
+      {{ category }}
+    </v-chip>
   </div>
 </template>
 
@@ -17,10 +29,21 @@ import { mapState } from 'vuex'
 export default {
   name: 'CategoriesList',
   data() {
-    return {}
+    return {
+      selectedCategories: []
+    }
   },
   created() {
     this.$store.dispatch('songs/getCategories')
+  },
+  methods: {
+    toggleCategory(category) {
+      if (this.selectedCategories.indexOf(category) >= 0) {
+        this.selectedCategories.splice(this.selectedCategories.indexOf(category), 1)
+      } else {
+        this.selectedCategories.push(category)
+      } 
+    }
   },
   computed: {
     ...mapState('songs', [
