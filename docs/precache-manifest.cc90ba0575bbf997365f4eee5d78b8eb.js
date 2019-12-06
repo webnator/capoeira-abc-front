4f22c17753c372e1e11ea7e9966789c87d05583d
Williams Aguilera <webnator@gmail.com>
2019-12-06 11:56:26 +0100

    Preparing to publish

---
+++ docs/precache-manifest.cc90ba0575bbf997365f4eee5d78b8eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38976ad9b79e70ce2c3",
+    "url": "/capo-app/css/app.390749a0.css"
+  },
+  {
+    "revision": "704d8f00ae13d1b1e7ef",
+    "url": "/capo-app/css/chunk-vendors.79233745.css"
+  },
+  {
+    "revision": "71dbaf56386cf3e351641d888e4bcc03",
+    "url": "/capo-app/img/logo-blanco.71dbaf56.png"
+  },
+  {
+    "revision": "c2b31bd0eb0c0e1961e8f59d04eaf394",
+    "url": "/capo-app/index.html"
+  },
+  {
+    "revision": "c38976ad9b79e70ce2c3",
+    "url": "/capo-app/js/app.c3f95cbb.js"
+  },
+  {
+    "revision": "704d8f00ae13d1b1e7ef",
+    "url": "/capo-app/js/chunk-vendors.63513dca.js"
+  },
+  {
+    "revision": "dfe143ef86d80de4443558137397d710",
+    "url": "/capo-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/capo-app/robots.txt"
+  }
+]);
